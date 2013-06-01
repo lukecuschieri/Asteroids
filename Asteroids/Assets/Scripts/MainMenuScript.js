64864e461a64d5c0a868a04a553ec7645e49552e
Luke Cuschieri <lukecuschieri@gmail.com>
2013-06-01 17:06:38 +0200

    Main Menu Fixes
    Some calculations were applied to the menu and button scripts to centre
    them on different resolution.

--- Asteroids/Assets/Scripts/MainMenuScript.js
+++ Asteroids/Assets/Scripts/MainMenuScript.js
@@ -12,27 +12,38 @@ function Start() {
 }
 
 function OnGUI() {
-	GUI.DrawTexture(Rect(0, 0, background.width, background.height), background);
+	var posX = (Screen.width - background.width) / 2;
+	var posY = (Screen.height - background.height) / 2;
+	
+	GUI.DrawTexture(Rect(posX, posY, background.width, background.height), background, ScaleMode.StretchToFill);
 	
 	if (nameInput) {
-		GUI.Label(Rect(100, 570, 250, 25), "Please enter your name:");
+		posX = (Screen.width - 235) / 2;
 		
-		GameControllerScript.playerName = GUI.TextField(Rect(100, 600, 150, 25), GameControllerScript.playerName);
+		GUI.Label(Rect(posX, posY + 570, 250, 25), "Please enter your name:");
 		
-		if (GUI.Button(Rect(260, 600, 75, 25), "OK")) {
+		GameControllerScript.playerName = GUI.TextField(Rect(posX, posY + 600, 150, 25), GameControllerScript.playerName);
+		
+		if (GUI.Button(Rect(posX + 160, posY + 600, 75, 25), "OK")) {
 			if (GameControllerScript.playerName != "") {
 				Application.LoadLevel(1);
 			}
 		}
 		
+		if (GUI.Button(Rect(posX + 160, posY + 635, 75, 25), "Back")) {
+			nameInput = false;
+		}
+		
 	} else {
+		posX = (Screen.width - 100) / 2;
+		
 		// the new game button
-		if (GUI.Button(Rect(100, 540, 100, 30), "New Game")) {
+		if (GUI.Button(Rect(posX, posY + 540, 100, 30), "New Game")) {
 			nameInput = true;
 		}
 		
 		// the quit button
-		if (GUI.Button(Rect(100, 590, 100, 30), "Quit")) {
+		if (GUI.Button(Rect(posX, posY + 590, 100, 30), "Quit")) {
 			Application.Quit();
 		}
 	}
